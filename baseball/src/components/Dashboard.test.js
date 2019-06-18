e2f8412a1dc5a04b7ad72d51035a74c20cd1a71a
mjfalahee <mjfalahee@gmail.com>
2019-06-18 14:35:27 -0700

    Added the last test cases for resetting with strikes and balls

--- baseball/src/components/Dashboard.test.js
+++ baseball/src/components/Dashboard.test.js
@@ -28,6 +28,14 @@ describe("<Dashboard />", () => {
             fireEvent.click(button);
             expect(queryByText(/Count: 0-1/i)).toBeTruthy();
         })
+        it('resets the count to 0 when there are 3 strikes', () => {
+            const { getByText, queryByText} = render(<Dashboard />);
+            const button = getByText(/strike/i);
+            fireEvent.click(button);
+            fireEvent.click(button);
+            fireEvent.click(button);
+            expect(queryByText(/Count: 0-0/i)).toBeTruthy();
+        })
     })
 
     describe('Ball Button', () => {
@@ -36,6 +44,15 @@ describe("<Dashboard />", () => {
             const button = getByText(/ball/i);
             fireEvent.click(button);
             expect(queryByText(/Count: 1-0/i)).toBeTruthy();
+        })
+        it('resets the count to 0 when 4 balls are thrown', () => {
+                const { getByText, queryByText} = render(<Dashboard />);
+                const button = getByText(/ball/i);
+                fireEvent.click(button);
+                fireEvent.click(button);
+                fireEvent.click(button);
+                fireEvent.click(button);
+                expect(queryByText(/Count: 0-0/i)).toBeTruthy();
         })
     })
     describe('Foul button', () => {

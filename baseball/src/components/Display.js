bf856e0b9de4bb8252930a91261acef4bec18542
mjfalahee <mjfalahee@gmail.com>
2019-06-18 13:15:58 -0700

    Working on test cases now.

--- baseball/src/components/Display.js
+++ baseball/src/components/Display.js
@@ -9,8 +9,8 @@ export default class Display extends React.Component {
     render() {
         return (
             <>
-            <h3> Strikes: {this.props.strikes} </h3>
-            <h3> Balls: {this.props.balls}   </h3>
+            <h3>Strikes: {this.props.strikes}</h3>
+            <h3>Balls: {this.props.balls}</h3>
             </>
         )
     }
